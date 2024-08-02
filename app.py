0cf36cdf39f850c31e23dbf8a1f1928f82be5d8f
git <yokofujishiro.0418@gmail.com>
2024-08-02 20:37:43 +0900

    [add]add templates rendered by jinja2

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template # 追加
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html') # 変更
+
+if __name__ == "__main__":
+    app.run(debug=True)
