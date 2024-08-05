5745059fc456295f6319f772295120bf65549369
git <yokofujishiro.0418@gmail.com>
2024-08-05 11:39:22 +0900

    [add]Add routing to report page

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template # 追加
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html') # 変更
+    return render_template('index.html')
+
+@app.route('/report')
+def report():
+    return render_template('report.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
