22fa855d40313e38dfbb2ee1b0a2e9bda89c431a
git <yokofujishiro.0418@gmail.com>
2024-08-06 20:06:57 +0900

    [fix]add for loop to list episode and photo

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
-
+from collections import namedtuple
+from datetime import date
 app = Flask(__name__)
+
+Episode = namedtuple('Episode', ['number', 'title'])
+Photo = namedtuple('Photo', ['date', 'thumbnail'])
 
 @app.route('/')
 def index():
@@ -10,14 +14,28 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/episode/heji/latest')
+@app.route('/episode/heji/')
 def episode():
-    return render_template('episode.html')
+    episode_list=sorted([Episode(number=1,title="OPEN da DOOR(1)"),
+                         Episode(number=2,title="OPEN da DOOR(2)"),
+                         Episode(number=3,title="Big Dog's Digging LIFE")],reverse=True)
+
+    return render_template('episode.html',episode_list=episode_list)
 
 @app.route('/photos')
 def photos():
-    return render_template('photos.html')
+# TODO データベース内のすべてのサムネイルを検索する
+# TODO 時間順でサムネイルを並べる
+    photo_list=sorted([Photo(date=date(2019,7,30),
+                      thumbnail='static/photos/20190730-chikazu.png'),
+                Photo(date=date(2019,7,30),
+                      thumbnail='static/photos/20190730-hejichika.png'),],reverse=True)
 
+    return render_template('photos.html', photo_list=photo_list)
+
+# TODO POSTメソッドでphotoのidを受け渡す
+# TODO 写真は時間順で並べてから、隣接するレコードを検索する
+# TODO データベースとの連携
 @app.route('/photos/photo')
 def photo():
     return render_template('photo.html')
